b40ace84b811405bbc24a2440c8a2a04d4dae5ae
ToastyX <erick.cool321@gmail.com>
2023-05-27 02:48:56 -0500

    Initial Commit: Developing the foundation.

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // TODO: Recipients Email Address
+
+        // TODO: Senders Emails Address
+
+        // TODO: Local Host IP Address
+
+        // TODO: Properties Object
+
+        // TODO: Mail Server (SMTP)
+
+        // TODO: Default Session Object
+    }
+}
