5d8ac4024fc2540891777b7dc73138c9e0009ce6
ToastyyX <erick.cool321@icloud.com>
2023-08-09 18:00:42 -0500

    Initial Commit: Added Class

---
+++ src/Email.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Email {
+    private String sender;
+    private String recipient;
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getSender() {
+        return this.sender;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getRecipient() {
+        return this.recipient;
+    }
+}
